b13608315565556a81c82fed9d14edad83629f91
Yuito <aoi.yuito.ehou@gmail.com>
2026-04-14 22:50:20 +0600

    ui update

--- lua/vault/query.lua
+++ lua/vault/query.lua
@@ -399,7 +399,7 @@ local function hist_render()
       vim.api.nvim_buf_add_highlight(hist_state.buf, hist_state.ns, 'HistCursorLine', line_idx, 0, -1)
     else
       if rec.starred == 1 then
-        vim.api.nvim_buf_add_highlight(hist_state.buf, hist_state.ns, 'HistStarred', line_idx, 0, 1)
+        vim.api.nvim_buf_add_highlight(hist_state.buf, hist_state.ns, 'HistStarred', line_idx, 0, 3)
         --vim.api.nvim_buf_add_highlight(hist_state.buf, hist_state.ns, 'HistQuery',   line_idx, 2, -1)
       else
         --vim.api.nvim_buf_add_highlight(hist_state.buf, hist_state.ns, 'HistQuery', line_idx, 2, -1)
