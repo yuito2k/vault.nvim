157182d37f1ee6cfdc22124ee6efa1b3401945f4
Yuito <aoi.yuito.ehou@gmail.com>
2026-05-07 19:36:35 +0600

    didn't bind it to the parent buf

--- lua/vault/connection.lua
+++ lua/vault/connection.lua
@@ -1241,7 +1241,7 @@ function M.render_edit_connection_ui(db_id, record)
 
       local win = vim.api.nvim_open_win(ibuf, true, {
         relative = 'win',
-        win      = main_win,
+        win      = edit_state.main_win,
         row      = field.row,
         col      = field.col - 5,
         width    = field.width,
